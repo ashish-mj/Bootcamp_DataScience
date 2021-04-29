4cb0b38b4cc5c24c17f3ba68670a5f3c23e21c10
Ashish M J <ashm.jagadeesh@gmail.com>
2021-04-29 13:07:48 +0530

    Predict and evaluate model

--- linear_regression.py
+++ linear_regression.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn import metrics
 
 #Read Data
 data = pd.read_csv('Data/USA_Housing.csv')
@@ -11,8 +12,8 @@ print(data.head(30))
 print(data.info())
 print(data.describe())
 #sns.pairplot(data)
-sns.distplot(data['Price'])
-sns.heatmap(data.corr(),annot=True)
+#sns.distplot(data['Price'])
+#sns.heatmap(data.corr(),annot=True)
 
 
 print(data.columns)
@@ -24,7 +25,20 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.4, random_
 lm = LinearRegression()
 lm.fit(X_train, Y_train)
 print(lm.intercept_)
-print(lm.coef_)
+
+cdf = pd.DataFrame(lm.coef_,X.columns,columns=["Coeff"])
+print(cdf)
+
+
+prediction = lm.predict(X_test)
+
+#plt.scatter(Y_test,prediction)
+sns.distplot((Y_test-prediction))
+
+print(metrics.mean_absolute_error(Y_test, prediction))   #mae
+print(metrics.mean_squared_error(Y_test, prediction))    #mse
+print(np.sqrt(metrics.mean_squared_error(Y_test, prediction)))  #rmse
+
 
 
 
