c86d1318fe3536b4bce77fbb7684197c15e0a920
Ashish M J <ashm.jagadeesh@gmail.com>
2021-05-04 13:43:29 +0530

    train model and evaluate

--- logistic_regression_project.py
+++ logistic_regression_project.py
@@ -28,3 +28,21 @@ data['Age'].plot.hist(bins=30)
 #sns.jointplot(x="Age",y="Area Income",data=data)
 #sns.jointplot(x="Age",y="Daily Time Spent on Site",data=data,kind='kde')
 #sns.pairplot(data,hue='Clicked on Ad')
+
+
+X = data[['Daily Time Spent on Site',"Age","Area Income","Daily Internet Usage","Male"]]
+Y = data['Clicked on Ad']
+
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.3,random_state=101)
+
+logmodel = LogisticRegression()
+logmodel.fit(X_train,Y_train)
+predictions = logmodel.predict(X_test)
+
+
+print(classification_report(Y_test,predictions))
+print(confusion_matrix(Y_test,predictions))
+
+
+
+
