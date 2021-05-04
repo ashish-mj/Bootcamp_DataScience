3701a6b81fb9c95d2d0a991de80b91d6151781aa
Ashish M J <ashm.jagadeesh@gmail.com>
2021-05-04 10:23:30 +0530

    Preprocessing of data

--- logistic_regression.py
+++ logistic_regression.py
@@ -16,3 +16,31 @@ print(train.head())
 
 test = pd.read_csv('Data/titanic_test.csv')
 print(test.head())
+
+
+#sns.heatmap(train.isnull(),yticklabels=False,cbar=False,cmap='viridis')
+#sns.countplot(x='Survived',hue='Pclass',data=train)
+#sns.distplot(train['Age'].dropna(),kde=False,bins=30)
+#sns.boxplot(x='Pclass',y='Age',data=train)
+
+
+def impute_age(cols):
+    Age = cols[0]
+    Pclass = cols[1]
+    
+    if pd.isnull(Age):
+        
+        if Pclass == 1: 
+            return 37
+        elif Pclass == 2:
+            return 29
+        else:
+            return 24
+    else:
+        return Age
+    
+    
+train['Age'] = train[['Age','Pclass']].apply(impute_age,axis=1)
+train.drop('Cabin',axis=1,inplace=True)
+
+sns.heatmap(train.isnull(),yticklabels=False,cbar=False,cmap='viridis')
