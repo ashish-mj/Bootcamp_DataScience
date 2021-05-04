f6f30d1713609dbce3b5c01843786c0a008425b6
Ashish M J <ashm.jagadeesh@gmail.com>
2021-05-04 11:47:07 +0530

    training the model

--- logistic_regression.py
+++ logistic_regression.py
@@ -10,6 +10,16 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+
+
+
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+
 
 train = pd.read_csv('Data/titanic_train.csv')
 print(train.head())
@@ -43,4 +53,31 @@ def impute_age(cols):
 train['Age'] = train[['Age','Pclass']].apply(impute_age,axis=1)
 train.drop('Cabin',axis=1,inplace=True)
 
-sns.heatmap(train.isnull(),yticklabels=False,cbar=False,cmap='viridis')
+#sns.heatmap(train.isnull(),yticklabels=False,cbar=False,cmap='viridis')
+print(train.info())
+
+sex = pd.get_dummies(train['Sex'],drop_first=True)
+embark = pd.get_dummies(train['Embarked'],drop_first = True)
+train.drop(['Sex','Embarked','Name','Ticket'],axis=1,inplace=True)
+train = pd.concat([train,sex,embark],axis=1)
+train.drop('PassengerId',axis=1,inplace=True)
+print(train.head())
+
+
+X = train.drop('Survived',axis=1)
+Y = train['Survived']
+
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.3,random_state=101)
+
+logmodel = LogisticRegression()
+logmodel.fit(X_train,Y_train)
+predictions = logmodel.predict(X_test)
+print(predictions)
+print(classification_report(Y_test,predictions))
+print(confusion_matrix(Y_test,predictions))
+
+
+
+
+
+
